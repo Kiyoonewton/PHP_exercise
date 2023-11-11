033f26d5111c27affd3f3abfcf0f0200cc2bdeb4
Kolawole Isaac <Ikolawole@wazobia.tech>
2023-11-11 17:04:38 +0100

    Php_lesson and exercise

--- _practical-app/2.php
+++ _practical-app/2.php
@@ -1,45 +1,62 @@
-<?php include "functions.php" ?>
-<?php include "includes/header.php" ?>
+<?php include "functions.php"; ?>
+<?php include "includes/header.php"; ?>
 
-	<section class="content">
+<section class="content">
 
-		<aside class="col-xs-4">
+	<aside class="col-xs-4">
 
-	<?php Navigation();?>
-			
-			
-		</aside><!--SIDEBAR-->
+		<?php Navigation(); ?>
 
 
-		<article class="main-content col-xs-8">
-			<h2>Practice Section 1</h2>
+	</aside><!--SIDEBAR-->
 
 
-		<?php  
+	<article class="main-content col-xs-8">
 
+
+		<?php
+
+		$number1 = 10;
+		$number2 = 20;
+
+		$sum = $number1 + $number2;
+
+		echo "The Sum of Number1 And Number2 = " . $sum . "<br>";
+
+		$array1 = [1, 2, 3, 4];
+
+		$array2 = array(10, 20, 30, 40, 50);
+
+		$array = array("first_value" => "100", "second_value" => "200");
+
+
+		echo "First value of array1 = " . $array1[0] . "<br>";
+		echo "Second value array1 = " . $array1[1] . "<br>";
+		echo "Third value array1 = " . $array1[2] . "<br>";
+		echo "First value array2 = " . $array2[0] . "<br>";
+		echo "Second value array2  = " . $array2[1] . "<br>";
+		echo "Third value array2  = " . $array2[2] . "<br>";
+		echo "First value = " . $array["first_value"] . "<br>";
+		// print_r(array);
+		
 
 		/* Step 1: Make 2 variables called number1 and number2 and set 1 to value 10 and the other 20:
 
-		  Step 2: Add the two variables and display the sum with echo:
+					  Step 2: Add the two variables and display the sum with echo:
 
 
+					  Step3: Make 2 Arrays with the same values, one regular and the other associative
 
-		  Step2: Make 2 Arrays with the same values, one regular and the other associative
+					 
+						 */
 
-		  Step4: Make a constant and set it to the value of PHP.
 
-
-			
-
-			 */
-
-		
 
 
 		?>
 
-	
 
-		</article><!--MAIN CONTENT-->
 
-<?php include "includes/footer.php" ?>
+	</article><!--MAIN CONTENT-->
+
+	<?php include "includes/footer.php"; ?>
---
+++ new-lecture-files/variables copy.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $name = 'Edwin';
+    $number = 100;
+    $Number_List = 100;
+    //concatenation
+    echo $name . " " . $number;
+    $name = "<h1>Hello</h1>";
+    echo $name;
+    ?>
+</body>
+
+</html>
